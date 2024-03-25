f9424a13f00fd275c8af2940009d1fbf393407de
er1ck1h0r0 <162365651+er1ck1h0r0@users.noreply.github.com>
2024-03-25 10:13:11 +0300

    Code challenge Wk1

--- speed.js
+++ speed.js
@@ -1,15 +1,15 @@
 //Create a function called speedDetector
 function speedDetector(speed){
-    //If condition to check speed limit
+    //Create a condition that checks speed limit
     if (speed > 70){
         let demeritPoints = (speed-70)/5;
         if(demeritPoints < 12){
-        // Print the results when speed is less than 70.
-        console.log(`You are driving at a speed of ${speed} therefore your you have earned ${demeritPoints} demerit points.`)
+        // Return demerit points when speed is less than 70.
+        console.log(`You have earned ${demeritPoints} demerit points.`)
         }
         else {
-            //Print the results when speed is above 70
-            console.log(`You are driving at a speed of ${speed} therefore your you have earned ${demeritPoints} demerit points and your license has been suspended.`)
+            //Return the demerit points when speed is above 70
+            console.log(`You have earned ${demeritPoints} demerit points and your license has been suspended.`)
         }
     
     }
