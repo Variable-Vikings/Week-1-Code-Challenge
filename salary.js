b83f4fe174c9d30c22b71b0860a421c8af764406
er1ck1h0r0 <162365651+er1ck1h0r0@users.noreply.github.com>
2024-03-25 10:17:21 +0300

    Add files via upload

--- salary.js
+++ salary.js
@@ -1,17 +1,17 @@
 // define a function calculateNetSalary that calculates the salary
-function calaculateNetSalary(salary,benefits){
+function generateNetSalary(salary,benefits){
     //Parameters for salaries less than 24000
     if(salary <20000){
         let grossSalary = salary + benefits;
         let nhifContribution = 750;
         let nssfContribution = 0.06*grossSalary;
         let taxableIncome = grossSalary-nhifContribution - nssfContribution;
-        let paye = taxableIncome*0.1;
-        let netSalary= grossSalary-nhifContribution-nssfContribution-paye;
+        let payee = taxableIncome*0.1;
+        let netSalary= grossSalary-nhifContribution-nssfContribution-payee;
         
         console.log(`Gross Salary: Kshs${grossSalary}.`);
         console.log(`Taxable income: Kshs${taxableIncome}.`)
-        console.log(`Paye: Kshs${paye}`);
+        console.log(`Payee: Kshs${payee}`);
         console.log(`Nhif Contribution: Kshs${nhifContribution}.`);
         console.log(`Net Salary:Ksh${netSalary}`)
 
@@ -23,12 +23,12 @@ function calaculateNetSalary(salary,benefits){
         let nhifContribution = 850;
         let nssfContribution = 0.06*grossSalary;
         let taxableIncome = grossSalary-nhifContribution - nssfContribution;
-        let paye = taxableIncome*0.25; 
-        let netSalary=grossSalary-nhifContribution-nssfContribution-paye;
+        let payee = taxableIncome*0.25; 
+        let netSalary=grossSalary-nhifContribution-nssfContribution-payee;
 
         console.log(`Gross Salary: Kshs${grossSalary}.`);
         console.log(`Taxable income: Kshs${taxableIncome}.`)
-        console.log(`Paye: Kshs${paye}`);
+        console.log(`Payee: Kshs${payee}`);
         console.log(`Nhif Contribution: Kshs${nhifContribution}.`);
         console.log(`Net Salary: Ksh${netSalary}`)
     }
@@ -39,12 +39,12 @@ function calaculateNetSalary(salary,benefits){
         let nhifContribution = 1700;
         let nssfContribution = 0.06*grossSalary;
         let taxableIncome = grossSalary-nhifContribution-nssfContribution;
-        let paye = taxableIncome*0.3;
-        let netSalary=grossSalary-nhifContribution-nssfContribution-paye;
+        let payee = taxableIncome*0.3;
+        let netSalary=grossSalary-nhifContribution-nssfContribution-payee;
         
         console.log(`Gross Salary: Kshs${grossSalary}.`);
         console.log(`Taxable income: Kshs${taxableIncome}.`)
-        console.log(`Paye: Kshs${paye}`);
+        console.log(`Paye: Kshs${payee}`);
         console.log(`Nhif Contribution: Kshs${nhifContribution}.`);
         console.log(`Net Salary;Ksh${netSalary}`)
 
